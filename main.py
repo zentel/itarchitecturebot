f193e8e8bd73de071d5fa0d587825fbb7c15a034
Santiago <santiago.blanco@gmail.com>
2016-12-12 21:10:57 -0300

    Agregado setusername

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sqlite3
 import sys
 
 #Logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 #User parameters
@@ -27,8 +27,9 @@ def getParams(bot, update):
             dbConnection = sqlite3.connect(settings.database)
             cursor = dbConnection.cursor()
             query = "SELECT * FROM PARAMS WHERE CHAT_ID=" + str(update.message.chat_id) + ""
-            if cursor.rowcount > 0:
-                rows = cursor.fetchall()
+            rows = cursor.fetchall()
+            logger.warning(rows.count())
+            if rows.count() > 0:
                 for row in rows:
                     params[row["Param_Name"]] = row[["Param_Value"]]
             else:
@@ -70,9 +71,9 @@ def help(bot, update):
     msg = "*Commands:* \n"
     msg += "/projects - List your projects \n"
     msg += "/addproject <projectname> - Add a project \n"
-    msg += "/help - This message"
-    msg += "/setprojectportfolio <name> - set the project portfolio"
-    msg += "/setusername <name> - set the user name"
+    msg += "/help - This message  \n"
+    msg += "/setprojectportfolio <name> - set the project portfolio \n"
+    msg += "/setusername <name> - set the user name \n"
 
     # Send the message
     bot.send_message(chat_id=update.message.chat_id,
@@ -90,19 +91,55 @@ def setProjectPortfolio(bot, update, args):
         dbConnection = sqlite3.connect(settings.database)
         cursor = dbConnection.cursor()
         query = "SELECT * FROM PARAMS WHERE CHAT_ID=" + str(update.message.chat_id) + " and Param_Name='" + PROJECT_PORFOLIO + "';"
-
-        logger.debug("Tengo el cursor, para el query: {%s}", query)
         cursor.execute(query)
 
-        logger.debug("Ejecute el query de consulta, con rowcount {%d}", cursor.rowcount)
         if cursor.fetchone() is not None:
             query = "UPDATE PARAMS SET Param_Value='" + args[0] + "' WHERE CHAT_ID=" + str(update.message.chat_id) + " AND Param_Name ='" + PROJECT_PORFOLIO + "';"
         else:
             query = "INSERT INTO PARAMS (Chat_ID, Param_Name, Param_Value) VALUES(" + str(update.message.chat_id) + ",'" + PROJECT_PORFOLIO + "','" + args[0] + "');"
 
-        logger.debug(query)
         cursor.execute(query)
         dbConnection.commit()
+        msg = "By now, " + args[0] + " is your Project Portfolio \n"
+
+        # Send the message
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=msg,
+                         parse_mode=ParseMode.MARKDOWN)
+    except sqlite3.Error as e:
+        logger.error("%s", e.args[0])
+        sys.exit(1)
+    finally:
+        if dbConnection:
+            dbConnection.close()
+
+def setUserName(bot, update, args):
+    #Set the project portfolio
+    if len(args) <= 0:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text = "Please, use /setusername <name>")
+        return
+    dbConnection = None
+    try:
+        dbConnection = sqlite3.connect(settings.database)
+        cursor = dbConnection.cursor()
+        query = "SELECT * FROM PARAMS WHERE CHAT_ID=" + str(update.message.chat_id) + " and Param_Name='" + USER_NAME + "';"
+
+        cursor.execute(query)
+
+        if cursor.fetchone() is not None:
+            query = "UPDATE PARAMS SET Param_Value='" + args[0] + "' WHERE CHAT_ID=" + str(update.message.chat_id) + " AND Param_Name ='" + USER_NAME + "';"
+        else:
+            query = "INSERT INTO PARAMS (Chat_ID, Param_Name, Param_Value) VALUES(" + str(update.message.chat_id) + ",'" + USER_NAME + "','" + args[0] + "');"
+
+        cursor.execute(query)
+        dbConnection.commit()
+        msg = "By now, " + args[0] + " is your User Name \n"
+
+        # Send the message
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=msg,
+                         parse_mode=ParseMode.MARKDOWN)
     except sqlite3.Error as e:
         logger.error("%s", e.args[0])
         sys.exit(1)
@@ -113,7 +150,7 @@ def setProjectPortfolio(bot, update, args):
 def unknown(bot, update):
     #Unknown command handler
     bot.send_message(chat_id=update.message.chat_id,
-                     text="I'm sorry, I don't understand that command. Please use */help* for guidance.",
+                     text="I'm sorry, I don't understand that command. \nPlease use */help* for guidance.",
                      parse_mode=ParseMode.MARKDOWN)
 
 def error_callback(bot, update, error):
@@ -142,6 +179,7 @@ def error_callback(bot, update, error):
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('setprojectportfolio', setProjectPortfolio, pass_args=True))
+dispatcher.add_handler(CommandHandler('setusername', setProjectPortfolio, pass_args=True))
 dispatcher.add_error_handler(error_callback)
 dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
